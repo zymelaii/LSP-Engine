0e0e7ce9b84202993a03ab5834c0b380baf3ea30
ZYmelaii <13282135976@sina.cn>
2021-09-21 21:59:52 +0800

    Initial commit

--- hist/20210917/example/Collision/dummy.cpp
+++
@@ -1,5 +0,0 @@
-#define LSPE_IMPL
-#include "stable.h"
-
-template union lspe::vec2<float>;
-template class lspe::shape::Circle<float>;
--- hist/20210917/src/utils/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef LSPE_UTILS_H
-#define LSPE_UTILS_H
-
-#include "../utils/assert.h"
-#include "../utils/testunit.h"
-
-#endif /* LSPE_UTILS_H */
